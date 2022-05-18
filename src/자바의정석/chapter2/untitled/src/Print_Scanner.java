4fdc02a48645681787149c148a2217098ace29ab
developer-hyun <rlckdrkdud@naver.com>
2022-05-18 21:13:19 +0900

    feature-java-cheapter2-ver3

---
+++ src/자바의정석/chapter2/untitled/src/Print_Scanner.java
@@ -0,0 +1,7 @@
+public class Print_Scanner {
+    public static void main(String[] args) {
+
+        System.out.println(10.0/3);
+        System.out.printf("%s",Integer.toBinaryString(15));
+    }
+}
