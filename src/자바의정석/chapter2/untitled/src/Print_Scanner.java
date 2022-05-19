95e6ecbf78aa02271710d072cbb588f66be094a0
developer-hyun <rlckdrkdud@naver.com>
2022-05-19 17:29:57 +0900

    develop-feature-java-cheapter2-ver4

--- src/자바의정석/chapter2/untitled/src/Print_Scanner.java
+++ src/자바의정석/chapter2/untitled/src/Print_Scanner.java
@@ -1,7 +1,17 @@
+import java.util.Scanner;
+
 public class Print_Scanner {
     public static void main(String[] args) {
 
         System.out.println(10.0/3);
         System.out.printf("%s",Integer.toBinaryString(15));
+
+        Scanner scanner = new Scanner(System.in); //화면에 입력받기
+
+        int num = scanner.nextInt() ; //화면에서 입력받은 정수를 num에 저장
+
+        String input = scanner.nextLine(); //화면에서 입력받은 내용을 input에 저장
+
+        int num1 =Integer.parseInt(input);
     }
 }
