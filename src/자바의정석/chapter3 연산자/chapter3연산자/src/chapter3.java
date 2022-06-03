7d704af5c79dc7c18feead24bf6473fd9224072a
developer-hyun <rlckdrkdud@naver.com>
2022-06-03 12:23:41 +0900

    feature-java-chapter3-ver3

---
+++ src/자바의정석/chapter3 연산자/chapter3연산자/src/chapter3.java
@@ -0,0 +1,11 @@
+public class chapter3 {
+
+    public static void main(String[] args) {
+       // x << 1+2;
+        //System.out.println();
+        int i = 'B' - 'A';
+        System.out.println(i);
+
+
+    }
+}
