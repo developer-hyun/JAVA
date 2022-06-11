dfbec5e25d940038ab9effa23d9f256a75e20ec3
developer-hyun <rlckdrkdud@naver.com>
2022-06-11 20:00:35 +0900

    feature-java-chapter5-ver4

--- src/자바의정석/chapter5 배열/chapter5_배열/src/array.java
+++ src/자바의정석/chapter5 배열/chapter5_배열/src/array.java
@@ -3,5 +3,11 @@ public class array {
         int[] score = {100,90,80,70,60} ;
         int[] score1 = new int[] {100,90,80,70,60};
 
+        System.out.println("매개변수의 개수:" + args.length);
+        for (int i=0;i<args.length;i++) {
+            System.out.println("args["+i+"] =\"");
+        }
+
+
     }
 }
