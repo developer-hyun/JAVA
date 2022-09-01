8053309cf813f5a5a07eca3975dfa9a260512856
developer-hyun <rlckdrkdud@naver.com>
2022-09-01 17:06:36 +0900

    develop-feature-java-chapter14-ver2

--- src/자바의정석/Re_chapter6/Re_chapter6.java
+++ src/자바의정석/Re_chapter6/Re_chapter6.java
@@ -1,7 +1,24 @@
 public class Re_chapter6 {
+    public static void main(String[] args) {
 
+    }
+
+    static long fact(int n) {
+        long result = 0;
+        if (n==1) {
+            result = 1;
+        }
+        else {
+            result = n * fact(n - 1);
+        }
+        return result;
+    }
 }
 
 class Test {
+
+    Test() {}
+
+
 
 }
